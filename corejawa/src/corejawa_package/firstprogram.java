65358aab5fc439eb3823fde3217e3427f4d6b606
Laptop <Laptop@DESKTOP-87N756I>
2022-09-14 13:04:40 +0530

    First commit

---
+++ corejawa/src/corejawa_package/firstprogram.java
@@ -0,0 +1,10 @@
+package corejawa_package;
+
+public class firstprogram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ corejawa/src/module-info.java
@@ -0,0 +1,2 @@
+module corejawa {
+}
